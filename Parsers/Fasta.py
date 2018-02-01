a73c1b32916d613264fc382a5d8a942c4121dd0b
mccannj9 <jamcc09@gmail.com>
2018-02-01 20:15:44 +0100

    Finished theta estimators, added gitignore and Fasta Parserwq

---
+++ Parsers/Fasta.py
@@ -0,0 +1,142 @@
+#! /usr/bin/env python3
+
+
+class Sequence:
+
+	"""Sequence object that can be used for generating sequence objects
+	   from fasta files, things can be added as needed for other applications"""
+
+	def __init__(self, name="", seq="", qual="", seqform=""):
+		self.name = name
+		self.seq = seq
+		self.qual = qual
+		self.seqform = seqform
+		self.is_revcomp = False
+
+	def check_duplicate(self, other):
+		return self.seq == other.seq
+
+	def reverse_complement(self):
+		self.seq = reverseCompSequence(self.seq)
+		self.qual = reverseSequence(self.qual)
+		self.is_revcomp = not(self.is_revcomp)
+
+	def fasta_oneline(self):
+		return ">%s\n%s" % (self.name, self.seq)
+
+	def fasta_wrap(self, n=80):
+		seq_out = ">%s\n" % self.name
+		for x in range(0,len(self.seq),n):
+			seq_out += self.seq[x:x+n] + "\n"
+		seq_out = seq_out.rstrip('\n')
+		return seq_out
+
+	def fastq_out(self):
+		return "%s\n%s\n+\n%s" % (self.name,self.seq,self.qual)
+
+	def __str__(self):
+		return "name: %s\nseq: %s" % (self.name, self.seq)
+
+	def __repr__(self):
+		return "name: %s\nseq: %s" % (self.name, self.seq)
+
+	def __len__(self):
+		return len(self.seq)
+
+	def __eq__(self, other):
+		return self.name == other.name
+
+rclookup = {
+    'a': 't', 't': 'a', 'g': 'c', 'c': 'g',
+    'w': 'w', 's': 's', 'm': 'k', 'k': 'm', 'r': 'y', 'y': 'r',
+    'b': 'v', 'd': 'h', 'h': 'd', 'v': 'b', 'n': 'n',
+    'A': 'T', 'T': 'A', 'G': 'C', 'C': 'G',
+    'W': 'W', 'S': 'S', 'M': 'K', 'K': 'M', 'R': 'Y', 'Y': 'R',
+    'B': 'V', 'D': 'H', 'H': 'D', 'V': 'B', 'N': 'N', "-": "-"}
+
+def reverseCompSequence(sequence):
+    """
+    Defines a generic method for reverse complementing a sequence of
+    nucleotide codes.  This method fully supports all of the IUPAC
+    ambiguity codes.
+    """
+    tmp = ""
+
+    for base in sequence[::-1]:
+        tmp += rclookup[base]
+
+    return tmp
+
+
+def reverseSequence(sequence):
+	return sequence[::-1]
+
+
+def complementSequence(sequence):
+	"""returns a complemented sequence"""
+
+	tmp = ""
+
+	for base in sequence:
+		tmp += rclookup[base]
+
+	return tmp
+
+def trim_read(fq_obj, trim=0):
+	if trim > 0:
+		fq_obj.seq = fq_obj.seq[trim:]
+	elif trim < 0:
+		fq_obj.seq = fq_obj.seq[::-1][trim:][::-1]
+	return fq_obj
+
+
+class FastaReader(object):
+	"""
+	Reader for FASTA files.
+	"""
+	def __init__(self, file, wholefile=False, keep_linebreaks=False, sequence_class=Sequence):
+		"""
+		file is a filename or a file-like object.
+		If file is a filename, then .gz files are supported.
+		If wholefile is True, then it is ok to read the entire file
+		into memory. This is faster when there are many newlines in
+		the file, but may obviously need a lot of memory.
+		keep_linebreaks -- whether to keep the newline characters in the sequence
+		"""
+		#if isinstance(file, basestring):
+		#	file = xopen(file)
+		file = open(file)
+		self.fp = file
+		self.sequence_class = sequence_class
+		self.delivers_qualities = False
+
+	def __iter__(self):
+		"""
+		Read next entry from the file (single entry at a time).
+
+		# TODO this can be quadratic since += is used for the sequence string
+		"""
+		name = None
+		seq = ''
+		for line in self.fp:
+			# strip() should also take care of DOS line breaks
+			line = line.strip()
+			if line and line[0] == '>':
+				if name is not None:
+					assert seq.find('\n') == -1
+					yield self.sequence_class(name, seq, None, "fasta")
+				name = line[1:]
+				seq = ''
+			else:
+				seq += line
+		if name is not None:
+			assert seq.find('\n') == -1
+			yield self.sequence_class(name, seq, None, "fasta")
+
+	def __enter__(self):
+		if self.fp is None:
+			raise ValueError("I/O operation on closed FastaReader")
+		return self
+
+	def __exit__(self, *args):
+		self.fp.close()
---
+++ ThetaEstimators.py
@@ -0,0 +1,76 @@
+#! /usr/bin/env python
+
+from Parsers.Fasta import FastaReader
+
+
+def compute_pairwise_differences(seq1, seq2):
+    zipper = zip(seq1,seq2)
+
+    diffs = 0
+    for p,q in zipper:
+        if p != q:
+            diffs += 1
+
+    return diffs
+
+
+def compute_tajima_estimator(seqlist):
+    import itertools
+
+    seqs_pairwise = itertools.combinations(seqlist, 2)
+
+    number_comparisons = len(seqlist)*(len(seqlist) - 1)/2
+    diffs_pairwise = 0
+
+    for seq1, seq2 in seqs_pairwise:
+        diffs_pairwise += compute_pairwise_differences(seq1, seq2)
+
+    # this is averaged over sequence length
+    # how DNASP calculates it (nucleotide diversity)
+    return diffs_pairwise/(len(seqlist[0])*number_comparisons)*100
+
+
+def compute_watterson_estimator(seqs):
+    """ S = number of segregating sites
+        n = the sample size """
+
+    n = len(seqs)
+    S = 0
+
+    # seqs should obviously be all the same length
+    sequence_length = len(seqs[0])
+
+    for x in range(sequence_length):
+        position = []
+        for seq in seqs:
+            position.append(seq[x])
+        unique_bases = set(position)
+
+        if len(unique_bases) == 2:
+            S += 1
+
+        position = []
+
+    print("S = %s" % S)
+
+    # this is averaged over sequence length
+    # how DNASP calculates it (Watterson's Estimator)
+    return S/(sum([1/i for i in range(1,n)])*sequence_length)*100
+
+
+def main():
+    fasta_file = "/mnt/Data/Files/OpenCourseWare/PopGenData/TNFSF5_aligned.fas"
+    fasta = FastaReader(fasta_file)
+
+    seqs = []
+    for record in fasta:
+        seqs.append(record.seq)
+
+    theta_d = compute_tajima_estimator(seqs)
+    print("Td = %s" % theta_d)
+    theta_w = compute_watterson_estimator(seqs)
+    print("Ts = %s" % theta_w)
+
+
+if __name__ == '__main__':
+    main()
