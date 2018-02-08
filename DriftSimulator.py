04908352d73eefed66d0cffe8229d3f997448aa5
jamc <jamcc09@gmail.com>
2018-02-08 23:06:19 +0100

    Fixed some PEP8 errors.

--- DriftSimulator.py
+++ DriftSimulator.py
@@ -2,9 +2,10 @@
 
 import random
 
+
 def population_simulation(pop_size, num_gens, init_freq=0.5):
 
-    trials = 2*pop_size
+    trials = 2 * pop_size
 
     # adding initial freq so all sims start at same value in plot
     allele_freqs = [init_freq]
@@ -12,10 +13,10 @@ def population_simulation(pop_size, num_gens, init_freq=0.5):
     for x in range(num_gens):
         success = 0
         for y in range(trials):
-            if random.uniform(0,1) < init_freq:
+            if random.uniform(0, 1) < init_freq:
                 success += 1
 
-        init_freq = success/trials
+        init_freq = success / trials
         allele_freqs.append(init_freq)
 
     return allele_freqs
@@ -35,7 +36,7 @@ def main():
         simulations.append(freqs)
 
     # xdata for bokeh
-    xgenerations = [list(range(0,gens+1)) for y in range(nsims)]
+    xgenerations = [list(range(gens + 1)) for y in range(nsims)]
 
     plot_multiple_drift_simulations(
         xgenerations, simulations, "./Data/test.html"
