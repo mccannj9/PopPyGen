8ff9c6a00dd5fb103763015aaf7fa05b7e0c73e5
jamc <jamcc09@gmail.com>
2018-02-18 21:22:08 +0100

    Removed unnecessary vars, added func for converting to bin matrix

--- SiteFreqSpec.py
+++ SiteFreqSpec.py
@@ -8,11 +8,9 @@ def transpose_list_of_lists(matrix):
 
 
 def extract_segregating_sites(seqs, outgroup=0):
-    """ outgroup variable is the row number of the 
+    """ outgroup variable is the row number of the
         seq of the outgroup in the alignment   """
 
-    n = len(seqs)
-    S = 0
     seq_length = len(seqs[0])
 
     outgroup_seq = seqs[outgroup]
@@ -28,25 +26,38 @@ def extract_segregating_sites(seqs, outgroup=0):
         unique_bases = set(position)
         if len(unique_bases) == 2:
             if outgroup_seq[x] in unique_bases:
-                position.insert(0, outgroup[x])
+                position.insert(0, outgroup_seq[x])
                 site_matrix.append(position)
 
     return site_matrix
 
 
+def convert_to_binary_matrix(data_matrix):
+
+    binmat = []
+    for site in data_matrix:
+        outgroup = site[0]
+        position = site[1:]
+        binrow = ["0" if base == outgroup else "1" for base in position]
+        binmat.append(binrow)
+
+    return binmat
+
 
 def main():
-    fasta_file = "/mnt/Data/Files/OpenCourseWare/PopGenData/TNFSF5_aligned.fas"
+    fasta_file = "/home/jamc/Data/GitHub/PopPyGen/Data/TNFSF5_All_aligned.fas"
     fasta = FastaReader(fasta_file)
 
     seqs = []
     for record in fasta:
         seqs.append(record.seq)
 
-    theta_d = compute_tajima_estimator(seqs)
-    print("Td = %s" % theta_d)
-    theta_w = compute_watterson_estimator(seqs)
-    print("Ts = %s" % theta_w)
+    site_matrix = extract_segregating_sites(seqs)
+    binary_matrix = convert_to_binary_matrix(site_matrix)
+    print(site_matrix)
+    print(binary_matrix)
+    print("SM: nrows = %s :: ncols = %s" % (len(site_matrix), len(site_matrix[0])))
+    print("BM: nrows = %s :: ncols = %s" % (len(binary_matrix), len(binary_matrix[0])))
 
 
 if __name__ == '__main__':
